e91b4a7a32c3649ec60317dd01b92ddb22d021d4
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-10 11:25:03 +0500

    Edited Game Environment

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,12 +1,13 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
 
 public class TicTacToe extends JPanel
 {
     JPanel sPanel = new JPanel();
     JPanel bPanel = new JPanel();
     JLabel title = new JLabel("Tic Tac Toe Game");
-    JLabel score1 = new JLabel("0");  JLabel score2 = new JLabel("0");
+    JLabel[][] scoreBoard = new JLabel[2][2];
     JButton[][] buttons = new JButton[3][3];
 
     public TicTacToe()
@@ -14,15 +15,30 @@ public class TicTacToe extends JPanel
         setLayout(new BorderLayout());
         sPanel.setLayout(new GridLayout(2, 2));
         bPanel.setLayout(new GridLayout(3, 3));
-        title.setFont(new Font("Times New Roman", Font.BOLD, 50));
+        setBackground(Color.pink);  sPanel.setBackground(Color.PINK);
 
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++)
-                buttons[i][j] = new JButton();
+        title.setFont(new Font("Times New Roman", Font.BOLD, 50));
         
-        sPanel.add(new JLabel("PLAYER 1 : O"));
-        sPanel.add(new JLabel("PLAYER 2 : X"));
-        sPanel.add(score1);  sPanel.add(score2);
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++){
+                buttons[i][j] = new JButton();
+                buttons[i][j].setBackground(Color.pink);
+            }
+        
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 2; j++){
+                scoreBoard[i][j] = new JLabel();
+                sPanel.add(scoreBoard[i][j]);
+                scoreBoard[i][j].setBackground(Color.PINK);
+                scoreBoard[i][j].setFont(new Font("Arial", Font.BOLD, 40));
+            }
+        
+        scoreBoard[0][0].setText("PLAYER 1 : O");
+        scoreBoard[0][1].setText("PLAYER 2 : X");
+        scoreBoard[1][0].setText("0");
+        scoreBoard[1][1].setText("0");
+        
+        
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
                 bPanel.add(buttons[i][j]);
