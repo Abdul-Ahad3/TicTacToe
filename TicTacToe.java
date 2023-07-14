5cb748e4df253e147a8d82e863ca7abe36d41531
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-14 17:47:57 +0500

    Removed scoreboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,8 +7,7 @@ public class TicTacToe extends JPanel
     JPanel gPanel = new JPanel();
     JLabel title = new JLabel("Tic Tac Toe Game");
     JLabel player1 = new JLabel("PLAYER 1 : O");
-    JLabel player2 = new JLabel("PLAYER 1 : X");
-    JLabel score1 = new JLabel("0");  JLabel score2 = new JLabel("0");
+    JLabel player2 = new JLabel("PLAYER 2 : X");
 
     @Override
     public void paintComponent(Graphics g)
@@ -31,14 +30,9 @@ public class TicTacToe extends JPanel
         title.setFont(new Font("Times New Roman", Font.BOLD, 50));
         player1.setFont(new Font("Times New Roman", Font.BOLD, 50));
         player2.setFont(new Font("Times New Roman", Font.BOLD, 50));
-        score1.setFont(new Font("Arial", Font.BOLD, 40));
-        score2.setFont(new Font("Arial", Font.BOLD, 40));
-        gPanel.repaint();
         
         sPanel.add(player1);  sPanel.add(player2);
-        sPanel.add(score1);  sPanel.add(score2);
         
         add(title, BorderLayout.NORTH);  add(sPanel, BorderLayout.SOUTH);
-        //add(gPanel, BorderLayout.CENTER);
     }
 }
