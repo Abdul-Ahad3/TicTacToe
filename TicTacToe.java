0a21723d2cf77662f0470330d58bdb19f81d0b87
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-16 14:02:22 +0500

    Removed the scoring and its respective functionality

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,14 +6,15 @@ public class TicTacToe extends JPanel
     JPanel sPanel = new JPanel();
     JPanel bPanel = new JPanel();
     JLabel title = new JLabel("Tic Tac Toe Game");
-    JLabel[][] scoreBoard = new JLabel[2][2];
+    JLabel player1 = new JLabel("PLAYER 1 : O");
+    JLabel player2 = new JLabel("PLAYER 2 : X");
     JButton[][] buttons = new JButton[3][3];
     boolean turn = true;  boolean clicks = true;  int score = 0;
 
     public TicTacToe()
     {
         setLayout(new BorderLayout());
-        sPanel.setLayout(new GridLayout(2, 2));
+        sPanel.setLayout(new BorderLayout());
         bPanel.setLayout(new GridLayout(3, 3));
         setBackground(Color.pink);  sPanel.setBackground(Color.PINK);
         title.setFont(new Font("Times New Roman", Font.BOLD, 50));
@@ -31,31 +32,12 @@ public class TicTacToe extends JPanel
                 buttons[x][y].addActionListener(e -> {
                     if(turn){buttons[X][Y].setText("O");  turn = !turn;  buttons[X][Y].setEnabled(false);}
                     else{buttons[X][Y].setText("X");  turn = !turn;  buttons[X][Y].setEnabled(false);}
-
-                if(buttons[0][0].getText() == "O" && buttons[0][1].getText() == "O" && buttons[0][2].getText() == "O")
-                {scoreBoard[1][0].setText(String.valueOf(score++));}
-                else if(buttons[1][0].getText() == "O" && buttons[1][1].getText() == "O" && buttons[1][2].getText() == "O")
-                {scoreBoard[1][0].setText(String.valueOf(score++));}
-                else if(buttons[2][0].getText() == "O" && buttons[2][1].getText() == "O" && buttons[2][2].getText() == "O")
-                {scoreBoard[1][0].setText(String.valueOf(score++));}
-                });
-            }
+            });
         
-        for(int i = 0; i < 2; i++)
-            for(int j = 0; j < 2; j++)
-            {
-                scoreBoard[i][j] = new JLabel();
-                sPanel.add(scoreBoard[i][j]);
-                scoreBoard[i][j].setBackground(Color.PINK);
-                scoreBoard[i][j].setFont(new Font("Arial", Font.BOLD, 40));
-            }
-            
-        scoreBoard[0][0].setText("PLAYER 1 : O");
-        scoreBoard[0][1].setText("PLAYER 2 : X");
-        scoreBoard[1][0].setText("0");
-        scoreBoard[1][1].setText("0");
+        sPanel.add(player1, BorderLayout.WEST);  sPanel.add(player2, BorderLayout.EAST);
         
         add(title, BorderLayout.NORTH);  add(sPanel, BorderLayout.SOUTH);
         add(bPanel, BorderLayout.CENTER);
     }
+}
 }
