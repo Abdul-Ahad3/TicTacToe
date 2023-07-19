3527b525394538a5aa87829f10be6536e8dd7b19
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-19 21:12:02 +0500

    Final stages-2

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,7 +10,6 @@ public class TicTacToe extends JPanel
     JLabel player2 = new JLabel("PLAYER 2 : X");
     JButton[][] buttons = new JButton[3][3];
     boolean turn = true;  boolean clicks = true;  int score = 0;
-    String[] win1 = {"O", "O", "O"};  String[] win2 = {"X", "X", "X"};
     int topX=0, mid1X=0, bottomX=0, leftX=0, mid2X=0, rightX=0, cross1X=0, cross2X=0;
     int topO=0, mid1O=0, bottomO=0, leftO=0, mid2O=0, rightO=0, cross1O=0, cross2O=0;
 
@@ -39,52 +38,52 @@ public class TicTacToe extends JPanel
                     else{buttons[X][Y].setText("X");  turn = !turn;  buttons[X][Y].setEnabled(false);}
                     
                     //Checks for text on buttons
-                    if (buttons[X][0].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[X][0].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[X][0].getText().equals("O")) {topO++;} 
+                    else if (buttons[X][0].getText().equals("X")) {topX++;}
 
-                    if (buttons[X][1].getText().equals("O")) {mid1O++;  leftO++;  cross1O++;} 
-                    else if (buttons[X][1].getText().equals("X")) {mid1X++;  leftX++;  cross1X++;}
+                    if (buttons[X][1].getText().equals("O")) {mid1O++;} 
+                    else if (buttons[X][1].getText().equals("X")) {mid1X++;}
 
-                    if (buttons[X][2].getText().equals("O")) {bottomO++;  leftO++;  cross2O++;} 
-                    else if (buttons[X][2].getText().equals("X")) {bottomX++;  leftX++;  cross2X++;}
+                    if (buttons[X][2].getText().equals("O")) {bottomO++;} 
+                    else if (buttons[X][2].getText().equals("X")) {bottomX++;}
 
-                    if (buttons[0][Y].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[0][Y].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[0][Y].getText().equals("O")) {leftO++;} 
+                    else if (buttons[0][Y].getText().equals("X")) {leftX++;}
 
-                    if (buttons[1][Y].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[1][Y].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[1][Y].getText().equals("O")) {mid2O++;} 
+                    else if (buttons[1][Y].getText().equals("X")) {mid2X++;}
 
-                    if (buttons[2][Y].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[2][Y].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[2][Y].getText().equals("O")) {rightO++;} 
+                    else if (buttons[2][Y].getText().equals("X")) {rightX++;}
 
-                    if (buttons[X][0].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[X][0].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[X][0].getText().equals("O") && X==Y) {cross1O++;} 
+                    else if (buttons[X][0].getText().equals("X") && X==Y) {cross1X++;}
 
-                    if (buttons[X][0].getText().equals("O")) {topO++;  leftO++;  cross1O++;} 
-                    else if (buttons[X][0].getText().equals("X")) {topX++;  leftX++;  cross1X++;}
+                    if (buttons[X][0].getText().equals("O") && X+Y==2) {cross2O++;} 
+                    else if (buttons[X][0].getText().equals("X") && X+Y==2) {cross2X++;}
 
                     if(topX == 3 || mid1X == 3 || bottomX == 3 || leftX == 3 || mid2X == 3 || rightX ==3 || cross1X == 3 || cross2X == 3){
                         JOptionPane.showMessageDialog(null, "Player 2 wins");
-                        revalidate();  repaint();
+                        
                         topX=0; mid1X=0; bottomX=0; leftX=0; mid2X=0; rightX=0; cross1X=0; cross2X=0;
                         topO=0; mid1O=0; bottomO=0; leftO=0; mid2O=0; rightO=0; cross1O=0; cross2O=0;
                     }else if(topO == 3 || mid1O == 3 || bottomO == 3 || leftO == 3 || mid2O == 3 || rightO ==3 || cross1O == 3 || cross2O == 3){
                         JOptionPane.showMessageDialog(null, "Player 1 wins");
-                        revalidate();  repaint();
+                        
                         topX=0; mid1X=0; bottomX=0; leftX=0; mid2X=0; rightX=0; cross1X=0; cross2X=0;
                         topO=0; mid1O=0; bottomO=0; leftO=0; mid2O=0; rightO=0; cross1O=0; cross2O=0;
                     }else if (topX + mid1X + bottomX + leftX + mid2X + rightX + cross1X + cross2X + topO + mid1O + bottomO + leftO + mid2O + rightO + cross1O + cross2O == 24) {
                         JOptionPane.showMessageDialog(null, "It's a tie!");
-                        revalidate();  repaint();
+                        
                         topX=0; mid1X=0; bottomX=0; leftX=0; mid2X=0; rightX=0; cross1X=0; cross2X=0;
                         topO=0; mid1O=0; bottomO=0; leftO=0; mid2O=0; rightO=0; cross1O=0; cross2O=0;
                     }
-            });
+                });
+            }
         
         sPanel.add(player1, BorderLayout.WEST);  sPanel.add(player2, BorderLayout.EAST);
         
         add(title, BorderLayout.NORTH);  add(sPanel, BorderLayout.SOUTH);
         add(bPanel, BorderLayout.CENTER);
     }
-}
 }
